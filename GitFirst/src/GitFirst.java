54fa175f88f0a33ff9aac764f12db047583b6ec6
Isha <Isha@Isha-PC>
2018-11-17 13:47:43 +0530

    Scanner Method

--- GitFirst/src/GitFirst.java
+++ GitFirst/src/GitFirst.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class GitFirst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		System.out.print("The strig is: "+str);
 	}
 
 }
